c1c9dd1caf3e7a80fe4fbfeecce98bb4938c0488
Anzhelika Kostyuk <kostyukanzhelika@gmail.com>
2025-02-21 12:07:45 -0500

    Added new files for the front end

--- server.py
+++ server.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/<username>/<int:post_id>")
-def hello_world(username=None, post_id=None):
-    return render_template("./index.html", name=username, post_id=post_id)
+@app.route("/")
+def home():
+    return render_template("./index.html")
 
 
-@app.route("/about")
-def blog():
-    return render_template("./about.html")
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
